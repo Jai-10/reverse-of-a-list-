f72cf020efaf00cb7277dc4508f916156b4695c4
Jai-10 <71919287+Jai-10@users.noreply.github.com>
2020-09-30 20:29:43 +0530

    Modified the code to be simpler and has the ability to take input from the user

--- reverse list function.py
+++ reverse list function.py
@@ -1,5 +1,10 @@
-##slice list
-list=['P','Y','T','H','O','N','F','O','R','G','A','M','E','S']
-print("printing using :: ")
-sliced_list=list[::-1]
-print(sliced_list)
+# ##slice list
+# list=['P','Y','T','H','O','N','F','O','R','G','A','M','E','S']
+# print("printing using :: ")
+# sliced_list=list[::-1]
+# print(sliced_list)
+
+#This new code lets you reverse any string that has been input by the user.
+a = input()
+x = a[::-1]
+print(x)
